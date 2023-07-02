684ac8ff869215d2df38dcdf5374a7d9f4f2ce23
Aleksandar Lazrag <alex.lazrag@gmail.com>
2023-07-02 13:06:50 +0300

    Added an overlapping method so that Cars and reservation Dates don't overlap

--- src/main/java/com/finalproject/rentacar/repository/ReservationRepository.java
+++ src/main/java/com/finalproject/rentacar/repository/ReservationRepository.java
@@ -1,11 +1,13 @@
 package com.finalproject.rentacar.repository;
 
 import com.finalproject.rentacar.entity.Reservation;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
@@ -20,4 +22,16 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
             "OR " +
             "date_end BETWEEN :dateStart AND :dateEnd")
     Optional<Set<Reservation>> getReservationByIntervalWithNative(LocalDate dateStart, LocalDate dateEnd);
+
+    //Направих Query с цел да се провери от кога до кога е запазена една кола и да се провери дали при създаване на резервация
+    //се препокриват датите на резервиране на колата.
+    @Query(nativeQuery = true,
+            value =
+            "SELECT * FROM reservations " +
+            "WHERE car_id = :carId " +
+            "AND ((date_start <= :dateEnd) AND (date_end >= :dateStart))")
+    List<Reservation> findOverlappingReservations(@Param("carId") Long carId,
+                                                  @Param("dateStart") LocalDate dateStart,
+                                                  @Param("dateEnd") LocalDate dateEnd);
 }
+
--- src/main/java/com/finalproject/rentacar/service/impl/ReservationServiceImpl.java
+++ src/main/java/com/finalproject/rentacar/service/impl/ReservationServiceImpl.java
@@ -7,18 +7,16 @@ import com.finalproject.rentacar.dto.UpdateReservationCarRequest;
 import com.finalproject.rentacar.dto.UpdateReservationDatesRequest;
 import com.finalproject.rentacar.entity.Car;
 import com.finalproject.rentacar.entity.Reservation;
-
 import com.finalproject.rentacar.exceptions.DuplicateEntityException;
 import com.finalproject.rentacar.exceptions.NotFoundException;
 import com.finalproject.rentacar.repository.CarRepository;
 import com.finalproject.rentacar.repository.ReservationRepository;
-
 import com.finalproject.rentacar.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDate;
 import java.util.Collections;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -38,13 +36,29 @@ public class ReservationServiceImpl implements ReservationService {
     @Override
     public ReservationResponse bookReservation(ReservationRequest request) {
         Reservation reservation = reservationConverter.toReservation(request);
-        Reservation existingUID = reservationRepository.findByUserId(request.getUserId());
+        Reservation existingUserId = reservationRepository.findByUserId(request.getUserId());
+
+        //Имплементираме Query метода за преплитане на дати на резервация
+        List<Reservation> overlappingReservations = reservationRepository.findOverlappingReservations(
+                reservation.getCar().getId(),
+                reservation.getDateStart(),
+                reservation.getDateEnd()
+        );
 
         //Прави се проверка дали е направена резервация за с това userId, за да не се инкрементира reservationId ненужно.
-        if (existingUID != null){
-            throw new DuplicateEntityException("User with id '" + existingUID.getUser().getId() +
-                    "' has already reserved car with id '" + existingUID.getCar().getId() + "'");
+        if (existingUserId != null) {
+            throw new DuplicateEntityException("User with id '" + existingUserId.getUser().getId() +
+                    "' has already reserved car with id '" + existingUserId.getCar().getId() + "'");
 
+        }
+        //правим проверка ако колата, която се опитваме да резервираме е вече резервирана в този период от време
+        if (!overlappingReservations.isEmpty()) {
+            LocalDate conflictingStartDate = overlappingReservations.get(0).getDateStart();
+            LocalDate conflictingEndDate = overlappingReservations.get(0).getDateEnd();
+
+
+            throw new DuplicateEntityException("Car with id " + reservation.getCar().getId() + " is already reserved between "
+                    + conflictingStartDate + " and " + conflictingEndDate);
         }
 
         Reservation savedReservation = reservationRepository.save(reservation);
@@ -85,7 +99,27 @@ public class ReservationServiceImpl implements ReservationService {
         Reservation reservation = reservationRepository.findById(id).orElseThrow();
         Car car = carRepository.findById(request.getCarId()).orElseThrow();
 
-        if (request.getCarId() !=null && reservation.getCar().getId() != request.getCarId()){
+        //Добавяме пак overlapping метода с цел да проверим дали колата, която искаме да update-нем в резервацията, вече не е резервирана
+        // за датите зададени в резервацията.
+
+        List<Reservation> overlappingReservations = reservationRepository.findOverlappingReservations(
+                //използваме request вместо reservation, за да проверим колата, която искаме да сложим в съществуващата резервация
+                request.getCarId(),
+                reservation.getDateStart(),
+                reservation.getDateEnd()
+        );
+
+        if (!overlappingReservations.isEmpty()){
+            LocalDate conflictingStartDate = overlappingReservations.get(0).getDateStart();
+            LocalDate conflictingEndDate = overlappingReservations.get(0).getDateEnd();
+
+
+            throw new DuplicateEntityException("Car with id " + request.getCarId() + " is already reserved between "
+                    + conflictingStartDate + " and " + conflictingEndDate);
+
+        }
+
+        if (request.getCarId() != null && reservation.getCar().getId() != request.getCarId()) {
             reservation.setCar(car);
         }
         return reservationConverter.toResponse(reservationRepository.save(reservation));
@@ -94,6 +128,24 @@ public class ReservationServiceImpl implements ReservationService {
     @Override
     public ReservationResponse changeDates(Long id, UpdateReservationDatesRequest request) {
         Reservation reservation = reservationRepository.findById(id).orElseThrow();
+
+        //Добавяме overlapping метода, за проверка на датите на резервацията с цел да не се преплитат датите на резервация за една и съща кола.
+        List<Reservation> overlappingReservations = reservationRepository.findOverlappingReservations(
+                reservation.getCar().getId(),
+                request.getDateStart(),
+                request.getDateEnd()
+        );
+
+        if (!overlappingReservations.isEmpty()){
+            LocalDate conflictingStartDate = overlappingReservations.get(0).getDateStart();
+            LocalDate conflictingEndDate = overlappingReservations.get(0).getDateEnd();
+
+
+            throw new DuplicateEntityException("Car with id " + reservation.getCar().getId() + " is already reserved between "
+                    + conflictingStartDate + " and " + conflictingEndDate);
+
+        }
+
         reservation.setDateStart(request.getDateStart());
         reservation.setDateEnd(request.getDateEnd());
         return reservationConverter.toResponse(reservationRepository.save(reservation));
